0a4103e41b2a84622d3b6ea26c7782346632ef38
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-02-06 21:39:57 +0100

    Add cameras visualization

--- demo/draw_poses.py
+++ demo/draw_poses.py
@@ -4,6 +4,75 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+def cart2sph(x,y,z):
+    XsqPlusYsq = x**2 + y**2
+    elev = math.atan2(z,math.sqrt(XsqPlusYsq))     # theta
+    az = math.atan2(y,x)                           # phi
+    elev = elev / np.pi * 180
+    if elev < 0:
+        elev = 360 + elev
+    az = az / np.pi * 180
+    if az < 0:
+        az = 360 + az
+
+    return elev, az
+
+
+def fibonacci_sphere(samples=1000):
+
+    points = []
+    phi = math.pi * (math.sqrt(5.) - 1.)  # golden angle in radians
+
+    for i in range(samples):
+        y = 1 - (i / float(samples - 1)) * 2  # y goes from 1 to -1
+        radius = math.sqrt(1 - y * y)  # radius at y
+
+        theta = phi * i  # golden angle increment
+
+        x = math.cos(theta) * radius
+        z = math.sin(theta) * radius
+
+        points.append(cart2sph(x,y,z))
+
+    return points
+
+
+trans_t = lambda t : torch.Tensor([
+    [1,0,0,0],
+    [0,1,0,0],
+    [0,0,1,t],
+    [0,0,0,1]]).float()
+
+rot_phi = lambda phi : torch.Tensor([
+    [1,0,0,0],
+    [0,np.cos(phi),-np.sin(phi),0],
+    [0,np.sin(phi), np.cos(phi),0],
+    [0,0,0,1]]).float()
+
+rot_theta = lambda th : torch.Tensor([
+    [np.cos(th),0,-np.sin(th),0],
+    [0,1,0,0],
+    [np.sin(th),0, np.cos(th),0],
+    [0,0,0,1]]).float()
+
+rot_eta = lambda eta : torch.Tensor([
+    [np.cos(eta),np.sin(eta),0,0],
+    [-np.sin(eta), np.cos(eta),0,0],
+    [0,0,1,0],
+    [0,0,0,1]]).float()
+
+def pose_spherical(theta, phi, radius):
+    c2w = trans_t(radius)
+    c2w = rot_phi(phi/180.*np.pi) @ c2w
+    c2w = rot_theta(theta/180.*np.pi) @ c2w
+    eta = 180
+    c2w = rot_eta(eta / 180. * np.pi) @ c2w
+    c2w = torch.Tensor(np.array([[-1,0,0,0],
+                                 [0,0,1,0],
+                                 [0,1,0,0],
+                                 [0,0,0,1]])) @ c2w
+    return c2w.numpy()
+
 
 def read_txt_to_tensor(file_path):
     with open(file_path, 'r') as file:
@@ -21,36 +90,6 @@ def create_six_digit_strings():
     return six_digit_strings
 
 
-def pose_spherical(theta, phi, r):
-    x = r * np.sin(phi) * np.cos(theta)
-    y = r * np.sin(phi) * np.sin(theta)
-    z = r * np.cos(phi)
-    return x, y, z
-
-def fibonacci_sphere(samples=1000):
-    points = []
-    phi = math.pi * (3. - math.sqrt(5.))  # golden angle in radians
-
-    for i in range(samples):
-        y = 1 - (i / float(samples - 1)) * 2  # y goes from 1 to -1
-        radius = math.sqrt(1 - y * y)  # radius at y
-
-        theta = phi * i  # golden angle increment
-
-        x = math.cos(theta) * radius
-        z = math.sin(theta) * radius
-
-        points.append(cart2sph(x, y, z))
-
-    return points
-
-def cart2sph(x, y, z):
-    rho = np.sqrt(x**2 + y**2 + z**2)
-    phi = np.arccos(z / rho)
-    theta = np.arctan2(y, x)
-    return theta, phi, rho
-
-
 def main():
     ids = create_six_digit_strings()
 
@@ -63,19 +102,56 @@ def main():
         point = torch.tensor(point).float().view(4, 1)
         p_car = torch.matmul(pose, point)
         xyz_car = p_car.tolist()
-        # Extract first three numbers and convert to tuple
         xyz_car = tuple(xyz_car[:3])
         poses.append(xyz_car)
 
-    poses_multiplane = [pose_spherical(theta, phi, -1.307) for theta, phi, _ in fibonacci_sphere(6)]
+    poses_multiplane = []
+    poses_multiplane_flip = []
+    psphere = [pose_spherical(theta, phi, -1.307) for phi, theta in fibonacci_sphere(6)]
+
+    for p in psphere:
+        point = [0, 0, 0, 1]
+        point = torch.tensor(point).float().view(4, 1)
+        p_car = torch.matmul(torch.tensor(p), point)
+        xyz_car = p_car.tolist()
+        xyz_car = tuple(xyz_car[:3])
+        poses_multiplane.append(xyz_car)
+
+    for p in psphere:
+        point = [0, 0, 0, 1]
+        point = torch.tensor(point).float().view(4, 1)
+        pm = torch.tensor(p)
+        pm = pm @ torch.Tensor([[-1, 0, 0, 0],
+                                [0, 1, 0, 0],
+                                [0, 0, 1, 0],
+                                [0, 0, 0, 1]])
+
+        p_car = torch.matmul(pm, point)
+        xyz_car = p_car.tolist()
+        xyz_car = tuple(xyz_car[:3])
+        poses_multiplane_flip.append(xyz_car)
+
+
+    # for poses in poses_multiplane:
+    #     p = list(poses).copy()
+    #     p.append(1.0)
+    #     print(p)
+    #     p = torch.tensor(p).float().view(4, 1)
+    #     p = p @ torch.Tensor([[-1, 0, 0, 0], [0, 1, 0, 0],  [0, 0, 1, 0], [0, 0, 0, 1]])
+    #     p = p.tolist()
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
     for pose in poses:
         ax.scatter(*pose, color='b', s=5)
+        d = pose[0][0]**2 + pose[1][0]**2 + pose[2][0]**2
+        print(math.sqrt(d))
     for pose in poses_multiplane:
+
         ax.scatter(*pose, color='r', s=5)
+    for pose in poses_multiplane_flip:
+        ax.scatter(*pose, color='y', s=5)
 
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
