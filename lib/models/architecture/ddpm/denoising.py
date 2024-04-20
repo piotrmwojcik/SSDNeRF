24563f4740672478619e92aeca69639ec35f67e5
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-04-20 23:19:26 +0200

    big decoder

--- lib/models/architecture/ddpm/denoising.py
+++ lib/models/architecture/ddpm/denoising.py
@@ -304,6 +304,6 @@ class DenoisingUnetMod(DenoisingUnet):
             image_multi = image_multi.reshape(num_scenes, 3, 6, h, w)
 
             image_multi.requires_grad = True
-            image_multi.grad.copy_(outputs.grad.data.clone())
+            image_multi.grad = outputs.grad.data.clone()
 
         return image_multi
