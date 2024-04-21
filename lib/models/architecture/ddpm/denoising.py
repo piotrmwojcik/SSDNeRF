9d2d1366b0c176b14a5751c8869b97ba5f1e4215
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-04-21 15:42:13 +0200

    big decoder

--- lib/models/architecture/ddpm/denoising.py
+++ lib/models/architecture/ddpm/denoising.py
@@ -197,9 +197,6 @@ class DenoisingUnetMod(DenoisingUnet):
 
         code = code.reshape(code.size(0), *(3, 6, 128, 128))
 
-        print('!!!!')
-        print(code.shape)
-
         dt_gamma_scale = cfg.get('dt_gamma_scale', 0.0)
         # (num_scenes,)
         dt_gamma = dt_gamma_scale * 2 / (intrinsics[..., 0] + intrinsics[..., 1]).mean(dim=-1)
