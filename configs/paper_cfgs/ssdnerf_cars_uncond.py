facbf74b975d6921fea5c69024dc78c8d69f6ca6
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-04-22 00:14:38 +0200

    dupa

--- configs/paper_cfgs/ssdnerf_cars_uncond.py
+++ configs/paper_cfgs/ssdnerf_cars_uncond.py
@@ -1,5 +1,5 @@
 import os
-name = 'ssdnerf_cars_uncond'
+name = 'ssdnerf_cars_uncond_dupa'
 
 DATA_PATH = '/data/pwojcik/SSDNeRF/data/shapenet'
 
--- lib/models/architecture/ddpm/denoising.py
+++ lib/models/architecture/ddpm/denoising.py
@@ -208,6 +208,11 @@ class DenoisingUnetMod(DenoisingUnet):
         # forward middle blocks
         h = self.mid_blocks(h, embedding)
 
+        for name, param in self.mid_blocks.named_parameters():
+           if 'conv_2' in name:
+               print(name, param)
+               break
+
         # forward upsample blocks
         for block in self.out_blocks:
             h = block(torch.cat([h, hs.pop()], dim=1), embedding)
