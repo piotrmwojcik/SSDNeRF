80760db6a35594070470c690935189fd1613ab9a
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-04-21 17:14:18 +0200

    big decoder

--- lib/models/architecture/ddpm/denoising.py
+++ lib/models/architecture/ddpm/denoising.py
@@ -392,7 +392,7 @@ class DenoisingUnetMod(DenoisingUnet):
             pose_matrices = torch.stack(pose_matrices).repeat(num_scenes, 1, 1, 1).to(device)
             h, w = 128, 128
 
-            _, den_bitfield = self.get_density(decoder, outputs, cfg=self.test_cfg)
+            _, den_bitfield = self.get_density(decoder, outputs, cfg=dict())
             image_multi, depth_multi = self.render(decoder, outputs, den_bitfield, h, w, intrinsics, pose_matrices,
                                                    cfg=dict())  # (num_scenes, num_imgs, h, w, 3)
 
