fec759a57523c4d314831d65b08454b4f90a37d2
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-06-05 21:08:25 +0200

    mdf loss

--- lib/models/losses/mdfloss.py
+++ lib/models/losses/mdfloss.py
@@ -7,7 +7,7 @@ class MDFLoss(nn.Module):
     def __init__(self, saved_ds_path, device):
         super(MDFLoss, self).__init__()
 
-        self.Ds = torch.load(saved_ds_path, map_location=device)
+        self.Ds = torch.load(saved_ds_path)
 
         self.num_discs = len(self.Ds)
 
