ecca0c60c9e9e2846486f61aa04ca6447957dd23
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-06-06 00:10:43 +0200

    mdf loss

--- lib/models/autodecoders/base_nerf.py
+++ lib/models/autodecoders/base_nerf.py
@@ -490,8 +490,10 @@ class BaseNeRF(nn.Module):
                     pose_matrices.append(M)
 
                 pose_matrices = torch.stack(pose_matrices).repeat(num_scenes, 1, 1, 1).to(device)
+
                 image_multi, _ = self.render(
-                    decoder, code, density_bitfield, h, w, intrinsics, pose_matrices, cfg=cfg)
+                    decoder, code.clone().detach(), density_bitfield,
+                    h, w, intrinsics, pose_matrices, cfg=cfg)
 
                 def clamp_image(img, num_images):
                     images = img.permute(0, 1, 4, 2, 3).reshape(
