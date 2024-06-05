55fe055690250942f5bc998bbaa06073e4f246a8
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-06-05 23:40:17 +0200

    mdf loss

--- lib/models/autodecoders/base_nerf.py
+++ lib/models/autodecoders/base_nerf.py
@@ -501,11 +501,11 @@ class BaseNeRF(nn.Module):
                     return torch.round(images * 255) / 255
 
                 pred_imgs_multi = clamp_image(image_multi, poses.shape[0])
-                print('!!!')
-                print(imgs_consistency.shape)
-                print(pred_imgs_multi.shape)
+                imgs_consistency = imgs_consistency.reshape(imgs_consistency.shape[0] * imgs_consistency.shape[1],
+                                                            imgs_consistency.shape[2], imgs_consistency.shape[3])
+                imgs_consistency = imgs_consistency.permute(0, 3, 1, 2)
 
-                loss_consistency = self.mdfloss(pred_imgs_multi.view(), imgs_consistency)
+                loss_consistency = self.mdfloss(pred_imgs_multi, imgs_consistency)
                 loss_consistency_dict = dict(mdfloss=loss_consistency)
                 if prior_grad is not None:
                     if isinstance(code_, list):
